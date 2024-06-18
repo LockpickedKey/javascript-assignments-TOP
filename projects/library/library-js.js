585775543b33b97375cbd655044368771ec1280f
LockpickedKey <zyroisbored@gmail.com>
2024-06-18 17:58:53 +0700

    added the basic assignment functions.

--- projects/library/library-js.js
+++ projects/library/library-js.js
@@ -9,3 +9,21 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
+
+const myLibrary = [];
+const title = document.getElementById("title").value
+const author = document.getElementById("author").value
+const pages = document.getElementById("pages").value
+const read = document.getElementById("read").value
+
+function Book(title, author, pages, read){
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+}
+    
+function addBookToLibrary() {
+      let newBook = new Book(title, author, pages, read);
+      myLibrary.push(newBook);
+}
