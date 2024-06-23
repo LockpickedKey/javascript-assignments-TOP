601f79374fccdf8575479b0c687155df60dbe375
LockpickedKey <zyroisbored@gmail.com>
2024-06-23 21:01:10 +0700

    Added a clear form button.

--- projects/library/library-js.js
+++ projects/library/library-js.js
@@ -1,47 +1,78 @@
 const myLibrary = [];
+const form = document.querySelector('.form');
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
-const confirmBtn = formDialog.querySelector("#submitButton");
+const confirmButton = formDialog.querySelector("#submitButton");
+const clearButton = formDialog.querySelector("#clear-form-button")
 
 showButton.addEventListener("click", () => {
-      dialog.showModal();
+    dialog.showModal();
 });
 
 closeButton.addEventListener("click", () => {
-      dialog.close();
+    dialog.close();
 });
 
-function Book(title, author, pages, read){
-      this.title = title;
-      this.author = author;
-      this.pages = pages;
-      this.read = read;
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
-    
+
 function addBookToLibrary() {
-      const title = document.querySelector("#title").value;
-      const author = document.querySelector("#author").value;
-      const pages = document.querySelector("#pages").value;
-      const read = document.querySelector("#read").checked;
-      let newBook = new Book(title, author, pages, read);
-      console.log(newBook);
-      myLibrary.push(newBook);
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const read = document.querySelector("#read").checked;
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook);
+    myLibrary.push(newBook);
 }
 
-function validateForm() {
-      const title = document.querySelector("#title").value;
-      const author = document.querySelector("#author").value;
-      const pages = document.querySelector("#pages").value;
-      if(title == "") return false;
-      if(author == "") return false;
-      if(pages == "") return false;
+// const addError = (error) => {
+//     let errorText = document.createElement('div');
+//     errorText.textContent = `* Please enter an '${error.id}'`;
+//     errorText.classList.add('error-text');
+//     form.insertBefore(errorText, title);
+// }
+
+// const removeError = (error) => {
+// }
+
+// const validateForm = () => {
+//     const title = document.querySelector("#title");
+//     const author = document.querySelector("#author");
+//     const pages = document.querySelector("#pages");
+//     if (title.value == "") {
+// 		addError(title);
+//         return false;
+//     } else removeError(title);
+// 	if (author.value == "") {
+// 		addError(author);
+// 	    return false;
+//     } else removeError();
+// 	if (pages.value == "") {
+// 		addError(pages);
+//         return false;
+//     } else removeError();
+// }
+
+function clearForm() {
+	title.value = "";
+	author.value = "";
+	pages.value = "";
 }
 
-confirmBtn.addEventListener("click", (event) => {
-      if (validateForm() == false) return;
-      event.preventDefault();
-      addBookToLibrary();
-      /*update table*/
-      dialog.close(); 
+confirmButton.addEventListener("click", (event) => {
+    if (validateForm() == false) return;
+    event.preventDefault();
+    addBookToLibrary();
+    /*update table*/
+    dialog.close();
+});
+
+clearButton.addEventListener("click", (event) => {
+	clearForm();
 });
